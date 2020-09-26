cd3eb0970a99c8457069b34c734a62ecc3092bf9
Yash Chaudhari <chaudhariyash10@gmail.com>
2020-09-26 12:57:56 +0530

    Added ans to que in more infosection

--- src/que.js
+++ src/que.js
@@ -1,15 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 class Que extends React.Component {
   constructor({ props }) {
     super(props);
-
-    this.state = { ans: false };
+    this.state = { isAns: false };
+    this.showAns = this.showAns.bind(this);
   }
+
+  showAns() {
+    
+    this.setState(state => ({
+      state: !state.isAns
+    }));
+   
+  }
+
   render() {
-    return <button>{this.props.question}</button>;
+    let division;
+    if (this.state.ans) {
+      division = (
+        <div>
+          <button onClick={this.showAns}>{this.props.question}</button>
+          <h2>{this.props.answer}</h2>
+        </div>
+      );
+    } else {
+      division = (
+        <div>
+          <button onClick={this.showAns}>{this.props.question}</button>
+        </div>
+      );
+    }
+
+    return division;
   }
 }
+
+const answers = [
+  "You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!",
+  "You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price.There's always something new to discover, and new TV shows and movies are added every week!",
+  "Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.",
+  "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
+  "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.",
+  "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.",
+];
+
 const ids = [1, 2, 3, 4, 5, 6];
 
 const questions = [
@@ -21,18 +57,9 @@ const questions = [
   "Is Netflix good for kids ?",
 ];
 
-const answers = [
-  "You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!",
-  "You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price.There's always something new to discover, and new TV shows and movies are added every week!",
-  "Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.",
-  "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
-  "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.",
-  "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.",
-];
-
 for (let i = 0; i < 6; i++) {
   ReactDOM.render(
-    <Que question={questions[i]} />,
+    <Que question={questions[i]} answer={answers[i]} />,
     document.getElementById(`que${ids[i]}`)
   );
 }
